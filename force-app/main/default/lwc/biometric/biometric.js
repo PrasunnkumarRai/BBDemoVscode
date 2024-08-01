7d9052dd83dfb7676ed291f00b9c329175e2c8e5
Abhay Pratap Singh <abhay.singh@mandsconsulting.com>
2024-08-01 05:57:16 +0530

    Prasuborg

---
+++ force-app/main/default/lwc/biometric/biometric.js
@@ -0,0 +1,36 @@
+import { LightningElement } from 'lwc';
+import { getBiometricsService } from 'lightning/mobileCapabilities';
+
+export default class NimbusPluginBiometricsService extends LightningElement {
+    status;
+    biometricsService;
+
+    connectedCallback() {
+      this.biometricsService = getBiometricsService();
+    }
+
+    handleVerifyClick() {
+      if (this.biometricsService.isAvailable()) {
+        const options = {
+          permissionRequestBody: "Required to confirm device ownership.",
+          additionalSupportedPolicies: ['PIN_CODE']
+        };
+        this.biometricsService.checkUserIsDeviceOwner(options)
+          .then((result) => {
+            // Do something with the result
+            if (result === true) {
+              this.status = "✔ Current user is device owner."
+            } else {
+              this.status = "𐄂 Current user is NOT device owner."
+            }
+          })
+          .catch((error) => {
+            // Handle errors
+            this.status = 'Error code: ' + error.code + '\nError message: ' + error.message;
+          });
+      } else {
+        // service not available
+        this.status = 'Problem initiating Biometrics service. Are you using a mobile device?';
+      }
+    }
+}
